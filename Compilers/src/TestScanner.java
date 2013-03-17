498a1db9a7dd3951fa7667691124a27a1f99dcc0
kingaM <kingaM@example.com>
2013-03-17 23:25:31 +0100

    Minor changes

--- Compilers/src/TestScanner.java
+++ Compilers/src/TestScanner.java
@@ -5,7 +5,7 @@ public class TestScanner {
 	public static void main(String args[]) throws Exception {
 
 		System.setIn(new FileInputStream("testfile.txt"));
-		//System.setIn(new FileInputStream("test06"));
+		//System.setIn(new FileInputStream("test03"));
 		Yylex myScanner = new Yylex(System.in);
 		QCup myParser = new QCup(myScanner);
 
