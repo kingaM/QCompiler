203eaa68dc59de798f1a28badcab05ea34543ae1
kingaM <kingaM@example.com>
2013-03-17 22:00:55 +0100

    Fixed error message

--- Compilers/src/TestScanner.java
+++ Compilers/src/TestScanner.java
@@ -4,8 +4,8 @@ public class TestScanner {
 
 	public static void main(String args[]) throws Exception {
 
-		//System.setIn(new FileInputStream("testfile.txt"));
-		System.setIn(new FileInputStream("test06"));
+		System.setIn(new FileInputStream("testfile.txt"));
+		//System.setIn(new FileInputStream("test06"));
 		Yylex myScanner = new Yylex(System.in);
 		QCup myParser = new QCup(myScanner);
 
