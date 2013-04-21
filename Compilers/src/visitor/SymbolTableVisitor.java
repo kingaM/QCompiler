beb89354e2f03778a4a41534834594f403c686b8
Mocanu Emil George <mocanuemil3006@yahoo.com>
2013-04-21 13:30:51 +0100

    switched type and id around

--- Compilers/src/visitor/SymbolTableVisitor.java
+++ Compilers/src/visitor/SymbolTableVisitor.java
@@ -92,7 +92,7 @@ public class SymbolTableVisitor implements Visitor {
 			String sig = "";
 			for ( int i = 0; i < fieldDecl.size(); i++ ) {
 				if(i>0)sig = sig + ";" + fieldDecl.get(i).getId() + ":" + fieldDecl.get(i).getType();
-				else sig = sig + fieldDecl.get(i).getId() + ":" + fieldDecl.get(i).getType();
+				else sig = sig + fieldDecl.get(i).getType() + ":" + fieldDecl.get(i).getId();
 			}
 			return sig;
 		}
